3b5d4d777e4ddb40f6d05adf021b494ef3a02791
YangHan-Morningstar <imuyanghan@gmail.com>
2020-07-25 16:58:10 +0800

    初始化

---
+++ COVID19-State/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVID19-State
+//
+//  Created by Tony Young on 2020/7/25.
+//  Copyright © 2020 YangHan-Morningstar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
